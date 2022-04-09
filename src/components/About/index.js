3d9ee17aa18d9e41218f55e85bdd194e6e11faba
kiran kumar <kirankumar76k>
2022-04-09 03:14:56 +0000

    routing practice

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="app-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+    />
+    <h1 className="heading">About</h1>
+    <p className="description">I Love to create! iam a frontend developer</p>
+  </div>
+)
+
+export default About
